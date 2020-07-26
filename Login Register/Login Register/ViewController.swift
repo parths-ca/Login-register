c5162dea1894d993e3c1404a8954af962978712d
parths-ca <65921876+parths-ca@users.noreply.github.com>
2020-07-26 12:36:38 -0700

    Add files via upload

---
+++ Login Register/Login Register/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login Register
+//
+//  Created by PARTH on 2020-07-22.
+//  Copyright © 2020 college. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
